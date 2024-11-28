e730eed5375f8e2401f447ecf98c0795fa91af79
CaduSilva28 <edu420092@gmail.com>
2024-11-28 19:55:11 -0300

    Usando apenas javascript puro e navegador

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import FindCustomerController from '../app/controllers/FindCustomerController';
-
-const routes = express.Router();
-
-routes.get('/customer', FindCustomerController.findCustomer);
-
-export default routes;
